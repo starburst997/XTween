4e7a0907cba71c37fad53b7ccd9dcc80dc93464f
Jean-Denis Boivin <jd@boiv.in>
2021-05-22 11:19:55 -0400

    Cleanup library for UPM

---
+++ Editor/Scripts/XTween/XTweenEditorManager.cs
@@ -0,0 +1,194 @@
+﻿/**********************************************************************************
+/*		File Name 		: XTweenEditorManager.cs
+/*		Author 			: R0biN
+/*		Description 	: 
+/*		Created Date 	: 2018-10-21
+/*		Modified Date 	: 
+/**********************************************************************************/
+
+using UnityEngine;
+using System;
+using System.IO;
+using System.Collections.Generic;
+using UnityEditor;
+
+namespace Toki.Tween
+{
+	[InitializeOnLoad]
+	public class XTweenEditorManager
+	{
+		/************************************************************************
+		*	 	 	 	 	Static Variable Declaration	 	 	 	 	 	    *
+		************************************************************************/
+		private static bool _initialized = false;
+		private static XTweenEditorManager _instance;
+		
+		/************************************************************************
+		*	 	 	 	 	Static Method Declaration	 	 	 	     	 	*
+		************************************************************************/
+		static XTweenEditorManager()
+		{
+			Instance.Initialize();
+		}
+
+		public static XTweenEditorManager Instance
+		{
+			get
+			{
+				if( _instance == null )
+				{
+					_instance = new XTweenEditorManager();
+				}
+				return _instance;
+			}
+		}
+
+		public static void UpdateEasingName()
+		{
+			List<EasingData> easingList = XTweenEditorData.Instance.easingDataList;
+			List<string> easingNameList = new List<string>();
+			easingList.ForEach(x => easingNameList.Add(x.name));
+			string[] names = easingNameList.ToArray();
+			string filePath = SystemUtil.AbsPath + "/Assets/Toki/XAssets/XTween/Scripts/EaseCustom.cs";
+
+			bool refresh = false;
+			if( File.Exists(filePath) )
+			{
+				List<string> scriptEnumList = new List<string>(XTweenDataUtil.GetEnumNameList<EaseCustom>());
+				if( scriptEnumList.Count == easingNameList.Count )
+				{
+					int length = easingNameList.Count;
+					for ( int i = 0; i < length; ++i )
+					{
+						if( !scriptEnumList.Contains(easingNameList[i]) )
+						{
+							refresh = true;
+							break;
+						}
+					}
+				}
+				else
+				{
+					refresh = true;
+				}
+			}
+			else
+			{
+				refresh = true;
+			}
+
+			if( refresh && false ) // TODO: Disabled, not sure if needed
+			{
+				string replaceStr = string.Join(",\n\t", names);
+				string path = SystemUtil.AbsPath + "/Assets/Toki/XAssets/XTween/Scripts/Editor/EaseCustomTemplete";
+				string content = FileReference.Read(path);
+				content = content.Replace("/* Name List */", replaceStr);
+				FileReference.Write(filePath, content);
+				XTweenEditorData.Instance.Save();
+				AssetDatabase.Refresh();
+			}
+		}
+
+		/************************************************************************
+		*	 	 	 	 	Private Variable Declaration	 	 	 	 	 	*
+		************************************************************************/
+		private PlayModeStateChange _playMode;
+		
+		/************************************************************************
+		*	 	 	 	 	Protected Variable Declaration	 	 	 	 	 	*
+		************************************************************************/
+		
+		/************************************************************************
+		*	 	 	 	 	Public Variable Declaration	 	 	 	 	 		*
+		************************************************************************/
+		public Action initializeListener;
+		
+		/************************************************************************
+		*	 	 	 	 	Getter & Setter Declaration	 	 	 	 	 		*
+		************************************************************************/
+		public bool Initialized
+		{
+			get
+			{
+				return _initialized;
+			}
+		}
+
+		/************************************************************************
+		*	 	 	 	 	Life Cycle Method Declaration	 	 	 	 	 	*
+		************************************************************************/
+		public XTweenEditorManager()
+		{
+			
+		}
+		
+		/************************************************************************
+		*	 	 	 	 	Coroutine Declaration	 	  			 	 		*
+		************************************************************************/
+		
+		/************************************************************************
+		*	 	 	 	 	Private Method Declaration	 	 	 	 	 		*
+		************************************************************************/
+		
+		/************************************************************************
+		*	 	 	 	 	Protected Method Declaration	 	 	 	 	 	*
+		************************************************************************/
+		
+		/************************************************************************
+		*	 	 	 	 	Event Method Declaration	 	 	 	     	 	*
+		************************************************************************/
+		private void ChangedPlayMode( PlayModeStateChange state )
+		{
+			if( this._playMode != state )
+			{
+				bool send = false;
+				switch ( state )
+				{
+					case PlayModeStateChange.EnteredEditMode:
+					case PlayModeStateChange.EnteredPlayMode:
+						send = true;
+						break;
+					default:
+						break;
+				}
+
+				this._playMode = state;
+				if( send ) XTween.PlayModeChanged( this._playMode.Equals(PlayModeStateChange.EnteredEditMode) );
+			}
+		}
+
+		private void CheckEditorData()
+		{
+			string destDir = "Assets/Resources";
+			string destPath = destDir + "/XTweenData.asset";
+			if( !File.Exists(SystemUtil.AbsPath + "/" + destPath) )
+			{
+				Directory.CreateDirectory(SystemUtil.AbsPath + "/" + destDir);
+				XTweenEditorData asset = ScriptableObject.CreateInstance<XTweenEditorData>();
+				AssetDatabase.CreateAsset (asset, destPath);
+				AssetDatabase.SaveAssets();
+				AssetDatabase.Refresh();
+			}
+
+			UpdateEasingName();
+		}
+
+		//============================== Ani ====================================
+		//============================== Net ====================================
+		//============================== UI =====================================
+		
+		/************************************************************************
+		*	 	 	 	 	Public Method Declaration	 	 	 	 	 		*
+		************************************************************************/
+		public void Initialize()
+		{
+			if( !_initialized )
+			{
+				_initialized = true;
+
+				EditorApplication.playModeStateChanged += this.ChangedPlayMode;
+				this.CheckEditorData();
+			}
+		}
+	}
+}
---
+++ Runtime/Scripts/XTween/Pool.cs
@@ -0,0 +1,155 @@
+﻿/**********************************************************************************
+/*		File Name 		: PoolClass
+/*		Author 			: Robin
+/*		Description 	: 
+/*		Created Date 	: 2019-1-5
+/*		Modified Date 	: 
+/**********************************************************************************/
+
+using UnityEngine;
+using UnityEngineInternal;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Toki.Tween;
+
+namespace Toki
+{
+	public class Pool<T> where T : IDisposable, new()
+	{
+		/************************************************************************
+		*	 	 	 	 	Static Variable Declaration	 	 	 	 	 	    *
+		************************************************************************/
+		private static Pool<T> _instance;
+		
+		/************************************************************************
+		*	 	 	 	 	Static Method Declaration	 	 	 	     	 	*
+		************************************************************************/
+		public static Pool<T> Instance
+		{
+			get
+			{
+				if( _instance == null )
+				{
+					_instance = new Pool<T>();
+				}
+				return _instance;
+			}
+		}
+
+		public static void Initialize( int size )
+		{
+			Pool<T>.Instance.InitialCreate(size);
+		}
+
+		public static T Pop()
+		{
+			return Pool<T>.Instance.PopInstance();
+		}
+
+		public static bool Contains(T target)
+		{
+			return Pool<T>.Instance.ContainsInstance(target);
+		}
+
+		public static void Push( T target )
+		{
+			Pool<T>.Instance.PushInstance(target);
+		}
+
+		/************************************************************************
+		*	 	 	 	 	Private Variable Declaration	 	 	 	 	 	*
+		************************************************************************/
+		private Stack<T> _stack = new Stack<T>();
+		
+		/************************************************************************
+		*	 	 	 	 	Protected Variable Declaration	 	 	 	 	 	*
+		************************************************************************/
+			
+		/************************************************************************
+		*	 	 	 	 	Public Variable Declaration	 	 	 	 	 		*
+		************************************************************************/
+			
+		/************************************************************************
+		*	 	 	 	 	Getter & Setter Declaration	 	 	 	 	 		*
+		************************************************************************/
+		
+		/************************************************************************
+		*	 	 	 	 	Initialize & Destroy Declaration	 	 	 		*
+		************************************************************************/
+		
+		/************************************************************************
+		*	 	 	 	 	Life Cycle Method Declaration	 	 	 	 	 	*
+		************************************************************************/
+
+		/************************************************************************
+		*	 	 	 	 	Coroutine Declaration	 	  			 	 		*
+		************************************************************************/
+		
+		/************************************************************************
+		*	 	 	 	 	Private Method Declaration	 	 	 	 	 		*
+		************************************************************************/
+		
+		/************************************************************************
+		*	 	 	 	 	Protected Method Declaration	 	 	 	 	 	*
+		************************************************************************/
+		
+		/************************************************************************
+		*	 	 	 	 	Public Method Declaration	 	 	 	 	 		*
+		************************************************************************/
+		private int _count = 0;
+		public void InitialCreate( int size )
+		{
+			while( this._stack.Count < size )
+			{
+				T instance = new T();
+				this._stack.Push(instance);
+			}
+		}
+		private bool _test = false;//true;
+		private HashSet<Type> _checkTypes = new HashSet<Type>
+		{
+			typeof(SerialTween), typeof(ObjectTween)
+		};
+
+		public T PopInstance()
+		{
+			T instance;
+			if( this._stack.Count > 0 )
+			{
+				instance = this._stack.Pop();
+			}
+			else
+			{
+				instance = new T();
+			}
+			_count++;
+			return instance;
+		}
+
+		public void PushInstance(T target)
+		{
+			target.Dispose();
+			this._stack.Push(target);
+			_count--;
+		}
+
+		public bool ContainsInstance(T target)
+		{
+			return this._stack.Contains(target);
+		}
+	}
+
+	public static class PoolShorcut
+	{
+		public static void PoolPush<T>(this T target) where T : IDisposable, new()
+		{
+			Pool<T>.Push(target);
+		}
+
+		public static bool PoolContains<T>(this T target) where T : IDisposable, new()
+		{
+			return Pool<T>.Contains(target);
+		}
+	}
+}
